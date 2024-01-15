8259744b1acb79186bd6eab984913887e8032c5f
DieMeister <135468820+DieMeister@users.noreply.github.com>
2024-01-15 22:56:55 +0100

    updated welcome message to include country roles

--- greetings.py
+++ greetings.py
@@ -5,7 +5,7 @@ async def welcome_message(bot, member,):
     channel = bot.get_channel(channel_preChats_wilkommen)
     await channel.send(f'''
 Hallo <@{member.id}>, wilkommen im ButterflyHub.
-Um dir selbst Game-Roles zu geben schau gerne in '**Channels & Roles**'(Kanäle und Rollen) (ganz oben in der Seitenleiste) vorbei.
+Um dir selbst Game- oder Länder-Rollen zu geben schau gerne in '**Channels & Roles**'(Kanäle und Rollen) (ganz oben in der Seitenleiste) vorbei.
 Wenn du Fragen oder Probleme hast zieh gerne ein Ticket in <#{channel_preChats_staffSupport}>.
 Vorschläge, wie der Server (noch) besser werden kann gerne in <#{channel_textChannels_vorschläge}>.
 Ansonsten viel Spaß auf dem Server {emoji_butterfly}.
